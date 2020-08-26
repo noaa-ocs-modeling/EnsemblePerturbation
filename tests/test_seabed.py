945e940ce433eb86f5515e6d13934590ecae6c36
Zachary.Burnett <zachary.burnett@noaa.gov>
2020-08-26 11:55:23 -0400

    remove interpolation test

--- tests/test_seabed.py
+++ tests/test_seabed.py
@@ -11,7 +11,7 @@ class TestSeabedDescriptions(unittest.TestCase):
         super().__init__(*args, **kwargs)
         self.crs = CRS.from_epsg(32618)
 
-        self.surveys = NGDCSeabedDescriptions.all_surveys()[:20]
+        self.surveys = NGDCSeabedDescriptions.all_surveys()[:5]
 
         bounds = numpy.array([[-77, 39], [-75, 40]])
         transformer = Transformer.from_crs(CRS.from_epsg(4326), self.crs)
@@ -25,12 +25,6 @@ class TestSeabedDescriptions(unittest.TestCase):
         assert seabed.data.shape[0] > 0
         assert seabed.data.shape[1] == 14
         assert len(seabed.descriptions) > 0
-
-    def test_interpolation(self):
-        seabed = NGDCSeabedDescriptions(bounds=self.bounds,
-                                        surveys=self.surveys, crs=self.crs)
-
-        seabed.interpolate((-76.5, 39.2))
 
 
 if __name__ == '__main__':
