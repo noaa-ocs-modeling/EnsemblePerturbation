072cfb7e066d6610bb862640cbde7034ec67534b
Zachary.Burnett <zachary.burnett@noaa.gov>
2020-08-28 15:00:39 -0400

    rename

--- examples/compare_zeta.py
+++ examples/compare_zeta.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from adcircpy.validation import COOPS
 from geopandas import GeoDataFrame
 from matplotlib import pyplot
 import numpy
@@ -38,8 +39,8 @@ if __name__ == '__main__':
     stations = first_coldstart['fort.61.nc']
     mesh = first_coldstart['maxele.63.nc']
 
-    stations_within_mesh = stations[
-        stations.within(mesh.unary_union.convex_hull)]
+    stations_within_mesh = stations[stations.within(
+        mesh.unary_union.convex_hull.buffer(0.01))]
 
     nearest_mesh_vertices = []
     for station_index, station in stations_within_mesh.iterrows():
@@ -54,7 +55,7 @@ if __name__ == '__main__':
         nearest_mesh_point_index = mesh.cx[nearest_mesh_point.x,
                                            nearest_mesh_point.y].index.item()
         nearest_mesh_vertices.append(GeoDataFrame({
-            'name': station['name'],
+            'station': station['name'],
             'station_x': station.geometry.x,
             'station_y': station.geometry.y,
             'distance': distance
@@ -64,20 +65,18 @@ if __name__ == '__main__':
     axes = {}
     for station_index, (_, station) in enumerate(
             stations_within_mesh.iterrows()):
-        elevation_axis = figure.add_subplot(
-            len(stations_within_mesh) * 2, 1, station_index + 1,
+        zeta_axis = figure.add_subplot(
+            len(stations_within_mesh) * 2, 1, station_index * 2 + 1,
             sharex=sharing_axis)
-
         if sharing_axis is None:
-            sharing_axis = elevation_axis
-
+            sharing_axis = zeta_axis
         difference_axis = figure.add_subplot(
-            len(stations_within_mesh) * 2, 1, station_index + 2,
+            len(stations_within_mesh) * 2, 1, station_index * 2 + 2,
             sharex=sharing_axis)
 
         axes[station['name']] = {
-            'elevation': elevation_axis,
-            'difference': difference_axis
+            'zeta': zeta_axis,
+            'zeta_difference': difference_axis
         }
 
     rmses = {}
@@ -88,13 +87,15 @@ if __name__ == '__main__':
         for stage, datasets in stages.items():
             fort61_filename = output_directory / run_name / stage / 'fort.61.nc'
 
-            model_times = datasets['fort.63.nc']['time']
-            modeled_ssh = datasets['fort.63.nc']['data']['zeta']
+            coops_tidal_stations = COOPS.TidalStations()
 
-            nearest_modeled_ssh = {
-                nearest_mesh_vertex['name']: GeoDataFrame({
+            model_times = datasets['fort.63.nc']['time']
+            modeled_zeta = datasets['fort.63.nc']['data']['zeta']
+
+            nearest_modeled_zeta = {
+                nearest_mesh_vertex['station']: GeoDataFrame({
                     'time': model_times,
-                    'zeta': modeled_ssh[:, nearest_mesh_point_index],
+                    'zeta': modeled_zeta[:, nearest_mesh_point_index],
                     'distance': nearest_mesh_vertex['distance']
                 }, geometry=[nearest_mesh_vertex.geometry
                              for _ in model_times])
@@ -103,61 +104,82 @@ if __name__ == '__main__':
             }
 
             del model_times
-            del modeled_ssh
+            del modeled_zeta
 
-            differences = []
-            for station_index, (station_name, modeled_ssh) in enumerate(
-                    nearest_modeled_ssh.items()):
-                observed_ssh = parse_fort61_stations(fort61_filename,
-                                                     [station_name])
+            zeta_differences = []
+            for station_index, (station_name, modeled_zeta) in enumerate(
+                    nearest_modeled_zeta.items()):
+                coops_tidal_stations.add_station(station_name,
+                                                 modeled_zeta['time'].min(),
+                                                 modeled_zeta['time'].max())
+                coops_tidal_stations.station = station_name
 
-                difference = (modeled_ssh[['time', 'zeta']] -
-                              observed_ssh[['time', 'zeta']]).abs()
+                coops_observed_zeta = DataFrame({
+                    'time': coops_tidal_stations.datetime,
+                    'zeta': coops_tidal_stations.values
+                })
 
-                difference.columns = ['time_difference', 'zeta']
+                observed_zeta = parse_fort61_stations(fort61_filename,
+                                                      [station_name])
+                observed_zeta = parse_fort61_stations(fort61_filename,
+                                                      [station_name])
 
-                difference = difference[reversed(difference.columns)]
+                zeta_difference = (modeled_zeta[['time', 'zeta']] -
+                                   observed_zeta[['time', 'zeta']]).abs()
 
-                difference.insert(0, 'time', modeled_ssh['time'])
-                difference.insert(len(difference.columns) - 1, 'station',
-                                  station_name)
-                difference.insert(len(difference.columns), 'distance',
-                                  modeled_ssh['distance'])
+                zeta_difference.columns = ['time_difference', 'zeta']
 
-                differences.append(difference)
+                zeta_difference = zeta_difference[
+                    reversed(zeta_difference.columns)]
 
-                axes[station_name]['elevation'].plot(observed_ssh['time'],
-                                                     observed_ssh['zeta'],
-                                                     label=f'{run_name} {stage} observation')
-                axes[station_name]['elevation'].plot(modeled_ssh['time'],
-                                                     modeled_ssh['zeta'],
-                                                     label=f'{run_name} {stage} model')
-                axes[station_name]['difference'].plot(difference['time'],
-                                                      difference['zeta'],
-                                                      label=f'{run_name} {stage}')
+                zeta_difference.insert(0, 'time', modeled_zeta['time'])
+                zeta_difference.insert(len(zeta_difference.columns) - 1,
+                                       'station', station_name)
+                zeta_difference.insert(len(zeta_difference.columns),
+                                       'distance', modeled_zeta['distance'])
 
-            differences = pandas.concat(differences)
+                zeta_differences.append(zeta_difference)
+
+                zeta_axis = axes[station_name]['zeta']
+                difference_axis = axes[station_name]['zeta_difference']
+
+                zeta_axis.plot(observed_zeta['time'],
+                               observed_zeta['zeta'],
+                               label=f'{run_name} {stage} fort.61')
+                zeta_axis.plot(coops_observed_zeta['time'],
+                               coops_observed_zeta['zeta'],
+                               label=f'{run_name} {stage} CO-OPS')
+                zeta_axis.plot(modeled_zeta['time'],
+                               modeled_zeta['zeta'],
+                               label=f'{run_name} {stage} model')
+                difference_axis.plot(zeta_difference['time'],
+                                     zeta_difference['zeta'],
+                                     label=f'{run_name} {stage}')
+
+            zeta_differences = pandas.concat(zeta_differences)
 
             rmses[run_name][stage] = {
-                'zeta_rmse': numpy.sqrt((differences['zeta'] ** 2).mean()),
-                'mean_time_difference': differences['time_difference'].mean(),
-                'mean_distance': differences['distance'].mean()
+                'zeta_rmse': numpy.sqrt(
+                    (zeta_differences['zeta'] ** 2).mean()),
+                'mean_time_difference': zeta_differences[
+                    'time_difference'].mean(),
+                'mean_distance': zeta_differences['distance'].mean()
             }
 
     for station_name, station_axes in axes.items():
-        station_axes['elevation'].set_title(
-            f'station {station_name} elevation')
-        station_axes['difference'].set_title(
-            f'station {station_name} elevation difference')
-        station_axes['elevation'].set_ylabel('zeta (m)')
-        station_axes['difference'].set_ylabel('zeta difference (m)')
-        station_axes['elevation'].legend(bbox_to_anchor=(1, 1),
-                                         loc='upper left', fontsize='xx-small')
-        station_axes['difference'].legend(bbox_to_anchor=(1, 1),
-                                          loc='upper left',
-                                          fontsize='xx-small')
+        station_axes['zeta'].set_title(f'station {station_name} zeta',
+                                       loc='left')
+        station_axes['zeta_difference'].set_title(
+            f'station {station_name} zeta difference', loc='left')
+        station_axes['zeta'].set_ylabel('zeta (m)')
+        station_axes['zeta_difference'].set_ylabel('zeta difference (m)')
+        station_axes['zeta'].legend(bbox_to_anchor=(1, 1), loc='upper left',
+                                    fontsize='xx-small')
+        station_axes['zeta_difference'].legend(bbox_to_anchor=(1, 1),
+                                               loc='upper left',
+                                               fontsize='xx-small')
 
-    # pyplot.show()
+    pyplot.show()
 
     rmses = DataFrame({
         'run': list(rmses.keys()),
@@ -174,14 +196,14 @@ if __name__ == '__main__':
                   for run in rmses['run']]
 
     figure = pyplot.figure()
-    elevation_axis = figure.add_subplot(1, 1, 1)
-    elevation_axis.suptitle('RMSE')
+    zeta_axis = figure.add_subplot(1, 1, 1)
+    zeta_axis.suptitle('RMSE')
     for column in rmses:
         if 'zeta' in column:
-            elevation_axis.plot(mannings_n, rmses[column], label=f'{column}')
-    elevation_axis.set_xlabel('Manning\'s N')
-    elevation_axis.set_ylabel('zeta RMSE (m)')
-    elevation_axis.legend()
+            zeta_axis.plot(mannings_n, rmses[column], label=f'{column}')
+    zeta_axis.set_xlabel('Manning\'s N')
+    zeta_axis.set_ylabel('zeta RMSE (m)')
+    zeta_axis.legend()
 
     pyplot.show()
 
