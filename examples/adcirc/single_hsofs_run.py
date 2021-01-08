b2606168b6b0d059ee6c8aa4fb36989d42355f1c
zacharyburnettNOAA <zachary.burnett@noaa.gov>
2021-01-08 18:42:11 +0000

    use hera-specific files

--- examples/adcirc/single_hsofs_run.py
+++ examples/adcirc/single_hsofs_run.py
@@ -14,7 +14,8 @@ from ensemble_perturbation.configuration.job_script import HPC
 from ensemble_perturbation.utilities import repository_root
 
 DATA_DIRECTORY = repository_root() / 'examples/data'
-INPUT_DIRECTORY = DATA_DIRECTORY / 'input' / 'hsofs'
+#INPUT_DIRECTORY = DATA_DIRECTORY / 'input' / 'hsofs'
+INPUT_DIRECTORY = Path("/scratch2/COASTAL/coastal/save/Saeed.Moghimi/setups/nems_inp/hsofs_grid_v1/")
 OUTPUT_DIRECTORY = DATA_DIRECTORY / 'configuration' / 'hsofs'
 
 if __name__ == '__main__':
@@ -27,8 +28,8 @@ if __name__ == '__main__':
         start_time=datetime(2012, 10, 22, 6),
         duration=timedelta(days=14.5),
         interval=timedelta(hours=1),
-        atm=AtmosphericMeshEntry('../../forcings/hsofs/Wind_HWRF_SANDY_Nov2018_ExtendedSmoothT.nc'),
-        wav=WaveMeshEntry('../../forcings/hsofs/ww3.HWRF.NOV2018.2012_sxy.nc'),
+        atm=AtmosphericMeshEntry('/scratch2/COASTAL/coastal/save/Saeed.Moghimi/setups/nems_inp/hsofs_forcings/san_v2/inp_atmesh/SANDY_HWRF_HSOFS_Nov2018.nc'),
+        wav=WaveMeshEntry('/scratch2/COASTAL/coastal/save/Saeed.Moghimi/setups/nems_inp/hsofs_forcings/san_v2/inp_ww3/ww3.HWRF.NOV2018.2012_sxy.nc'),
         ocn=ADCIRCEntry(382),
     )
 
