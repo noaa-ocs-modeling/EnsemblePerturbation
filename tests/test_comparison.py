0901fb5b1e012aa250fa5be5ae9f419928e81507
zachary.burnett <zachary.burnett@noaa.gov>
2021-06-08 16:30:21 -0400

    use pytest

--- tests/test_comparison.py
+++ tests/test_comparison.py
@@ -1,35 +1,28 @@
-import unittest
-
-from ensemble_perturbation import repository_root
-
 from ensembleperturbation.outputs.comparison import (
     ObservationStationComparison,
     StationComparison,
     VirtualStationComparison,
 )
+from ensembleperturbation.utilities import repository_root
 
 ROOT_DIRECTORY = repository_root() / 'examples/data'
 INPUT_DIRECTORY = ROOT_DIRECTORY / 'input'
 OUTPUT_DIRECTORY = ROOT_DIRECTORY / 'output'
 
 
-class TestComparison(unittest.TestCase):
-    def test_observation(self):
-        comparison = ObservationStationComparison(
-            INPUT_DIRECTORY, OUTPUT_DIRECTORY, ['u', 'v', 'zeta']
-        )
+def test_observation():
+    comparison = ObservationStationComparison(
+        INPUT_DIRECTORY, OUTPUT_DIRECTORY, ['u', 'v', 'zeta']
+    )
 
-        assert isinstance(comparison, StationComparison)
-
-    def test_virtual_stations(self):
-        stations_filename = ROOT_DIRECTORY / 'virtual_stations.gpkg'
-
-        comparison = VirtualStationComparison(
-            INPUT_DIRECTORY, OUTPUT_DIRECTORY, ['u', 'v', 'zeta'], stations_filename
-        )
-
-        assert isinstance(comparison, StationComparison)
+    assert isinstance(comparison, StationComparison)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_virtual_stations():
+    stations_filename = ROOT_DIRECTORY / 'virtual_stations.gpkg'
+
+    comparison = VirtualStationComparison(
+        INPUT_DIRECTORY, OUTPUT_DIRECTORY, ['u', 'v', 'zeta'], stations_filename
+    )
+
+    assert isinstance(comparison, StationComparison)
