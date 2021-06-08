ad14d17be63ad129691158cec05da4cfeb058e9a
zachary.burnett <zachary.burnett@noaa.gov>
2021-06-08 16:29:13 -0400

    use pytest

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,12 +5,7 @@ from ensembleperturbation.parsing.adcirc import ADCIRC_OUTPUTS, parse_adcirc_out
 ADCIRC_OUTPUT_DIRECTORY = Path(__file__).parent / 'data/Shinnecock_Inlet_NetCDF_output'
 
 
-class TestParser(unittest.TestCase):
-    def test_parse_adcirc_output(self):
-        output_data = parse_adcirc_output(ADCIRC_OUTPUT_DIRECTORY)
+def test_parse_adcirc_output():
+    output_data = parse_adcirc_output(ADCIRC_OUTPUT_DIRECTORY)
 
-        assert all(data_variable in output_data for data_variable in ADCIRC_OUTPUTS)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    assert all(data_variable in output_data for data_variable in ADCIRC_OUTPUTS)
