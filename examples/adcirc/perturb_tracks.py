e982c281c80d19f1f8f2bf9300a43f833fff3adc
zachary.burnett <zachary.burnett@noaa.gov>
2021-06-16 12:17:17 -0400

    use shared directory

--- examples/adcirc/perturb_tracks.py
+++ examples/adcirc/perturb_tracks.py
@@ -22,7 +22,7 @@ from ensembleperturbation.utilities import get_logger
 LOGGER = get_logger('perturb.adcirc')
 
 SHARED_DIRECTORY = Path('/scratch2/COASTAL/coastal/save/shared')
-OUTPUT_DIRECTORY = Path('/scratch2/COASTAL/coastal/save/shared/working/zach/adcirc')
+OUTPUT_DIRECTORY = SHARED_DIRECTORY / 'working' / 'zach' / 'adcirc'
 
 if not OUTPUT_DIRECTORY.exists():
     OUTPUT_DIRECTORY.mkdir(parents=True, exist_ok=True)
