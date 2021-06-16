0db2dc7ea64f8fa1b3ccefc12cc36ad6ad6b3340
zachary.burnett <zachary.burnett@noaa.gov>
2021-06-16 12:16:17 -0400

    consolidate tests

--- tests/test_track_ensemble.py
+++ tests/test_track_ensemble.py
@@ -1,7 +1,38 @@
 from dateutil.parser import parse as parse_date
 
+from ensembleperturbation.perturbation.make_storm_ensemble import (
+    AlongTrack,
+    CrossTrack,
+    MaximumSustainedWindSpeed,
+    RadiusOfMaximumWinds,
+    VortexPerturber,
+)
 from ensembleperturbation.tropicalcyclone.atcf import VortexForcing
 from tests import check_reference_directory, DATA_DIRECTORY
+
+
+def test_besttrack_ensemble():
+    output_directory = DATA_DIRECTORY / 'output' / 'test_besttrack_ensemble'
+    reference_directory = DATA_DIRECTORY / 'reference' / 'test_besttrack_ensemble'
+
+    if not output_directory.exists():
+        output_directory.mkdir(parents=True, exist_ok=True)
+
+    # hardcoding variable list for now
+    variables = [
+        MaximumSustainedWindSpeed,
+        RadiusOfMaximumWinds,
+        AlongTrack,
+        CrossTrack,
+    ]
+
+    perturber = VortexPerturber(storm='al062018', start_date='20180911', end_date=None)
+
+    perturber.write(
+        number_of_perturbations=3, variables=variables, directory=output_directory, alpha=0.5,
+    )
+
+    check_reference_directory(output_directory, reference_directory)
 
 
 def test_vortex_types():
