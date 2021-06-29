0db4ebc468516bac3a3768746a07212fa06cd04b
zachary.burnett <zachary.burnett@noaa.gov>
2021-06-29 10:32:19 -0400

    directories

--- tests/test_comparison.py
+++ tests/test_comparison.py
@@ -11,18 +11,24 @@ OUTPUT_DIRECTORY = ROOT_DIRECTORY / 'output'
 
 
 def test_observation():
+    input_directory = INPUT_DIRECTORY / 'test_parse_adcirc_output'
+    output_directory = input_directory
+
     comparison = ObservationStationComparison(
-        INPUT_DIRECTORY, OUTPUT_DIRECTORY, ['u', 'v', 'zeta']
+        input_directory, output_directory, ['u', 'v', 'zeta']
     )
 
     assert isinstance(comparison, StationComparison)
 
 
 def test_virtual_stations():
+    input_directory = INPUT_DIRECTORY / 'test_parse_adcirc_output'
+    output_directory = input_directory
+
     stations_filename = ROOT_DIRECTORY / 'virtual_stations.gpkg'
 
     comparison = VirtualStationComparison(
-        INPUT_DIRECTORY, OUTPUT_DIRECTORY, ['u', 'v', 'zeta'], stations_filename
+        input_directory, output_directory, ['u', 'v', 'zeta'], stations_filename
     )
 
     assert isinstance(comparison, StationComparison)
