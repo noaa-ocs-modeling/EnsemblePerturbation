41f10e6da0fd2d7bbab5a28ef9ff202ef5c6cf04
zachary.burnett <zachary.burnett@noaa.gov>
2021-08-02 08:23:23 -0400

    improvements to dependency installation in setup

--- setup.py
+++ setup.py
@@ -1,51 +1,95 @@
-#!/usr/bin/env python
 import importlib
 import logging
 import os
 from pathlib import Path
+import re
 import subprocess
 import sys
 
 from setuptools import config, find_packages, setup
 
-BUILT_PACKAGES = {'fiona': ['gdal'], 'numpy': [], 'pyproj': ['gdal'], 'shapely': ['gdal']}
-is_conda = (Path(sys.prefix) / 'conda-meta').exists()
+DEPENDENCIES = {
+    'adcircpy>=1.0.39': ['gdal', 'fiona'],
+    'appdirs': [],
+    'bs4': [],
+    'click': [],
+    'coupledmodeldriver>=1.4.6': [],
+    'fiona': ['gdal'],
+    'geopandas': [],
+    'matplotlib': [],
+    'nemspy>=0.6.16': [],
+    'netcdf4': [],
+    'numpy': [],
+    'pandas': [],
+    'pint': [],
+    'pint-pandas': [],
+    'pyproj>=2.6': [],
+    'python-dateutil': [],
+    'requests': [],
+    'shapely': [],
+}
 
-if is_conda:
-    conda_packages = []
-    for conda_package in BUILT_PACKAGES:
-        try:
-            importlib.import_module(conda_package)
-        except:
-            conda_packages.append(conda_package)
-    if len(conda_packages) > 0:
-        subprocess.check_call(['conda', 'install', '-y', *conda_packages])
 
-if os.name == 'nt':
-    for required_package, pipwin_dependencies in BUILT_PACKAGES.items():
-        try:
-            importlib.import_module(required_package)
-        except:
+def installed_packages() -> [str]:
+    return [
+        re.split('#egg=', re.split('==| @ ', package.decode())[0])[-1].lower()
+        for package in subprocess.check_output(
+            [sys.executable, '-m', 'pip', 'freeze']
+        ).splitlines()
+    ]
+
+
+def missing_packages(dependencies: {str: [str]}) -> {str: [str]}:
+    return {
+        dependency: subdependencies
+        for dependency, subdependencies in dependencies.items()
+        if re.split('<|<=|==|>=|>', dependency)[0].lower() not in installed_packages()
+    }
+
+
+missing_dependencies = missing_packages(DEPENDENCIES)
+
+if (Path(sys.prefix) / 'conda-meta').exists() and len(missing_dependencies) > 0:
+    try:
+        subprocess.check_call(['conda', 'install', '-y', list(missing_dependencies)])
+    except:
+        for dependency in list(missing_dependencies):
             try:
-                import pipwin
+                subprocess.check_call(['conda', 'install', '-y', dependency])
             except:
-                subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pipwin'])
+                continue
 
-            failed_pipwin_packages = []
-            for pipwin_package in pipwin_dependencies + [required_package]:
+    missing_dependencies = missing_packages(DEPENDENCIES)
+
+if os.name == 'nt' and len(missing_dependencies) > 0:
+    try:
+        import pipwin
+    except:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pipwin'])
+    subprocess.check_call([sys.executable, '-m', 'pipwin', 'refresh'])
+
+    for dependency, subdependencies in missing_dependencies.items():
+        failed_pipwin_packages = []
+        for _ in range(1 + len(subdependencies)):
+            for package_name in [dependency] + subdependencies:
                 try:
-                    subprocess.check_call(
-                        [sys.executable, '-m', 'pipwin', 'install', pipwin_package.lower()]
-                    )
-                except subprocess.CalledProcessError:
-                    failed_pipwin_packages.append(pipwin_package)
+                    importlib.import_module(package_name)
+                except:
+                    try:
+                        subprocess.check_call(
+                            [sys.executable, '-m', 'pipwin', 'install', package_name.lower()]
+                        )
+                        if package_name in failed_pipwin_packages:
+                            failed_pipwin_packages.remove(package_name)
+                    except subprocess.CalledProcessError:
+                        failed_pipwin_packages.append(package_name)
 
-            if len(failed_pipwin_packages) > 0:
-                raise RuntimeError(
-                    f'failed to download or install non-conda Windows build(s) of {" and ".join(failed_pipwin_packages)}; you can either\n'
-                    '1) install within an Anaconda environment, or\n'
-                    f'2) `pip install <file>.whl`, with `<file>.whl` downloaded from {" and ".join("https://www.lfd.uci.edu/~gohlke/pythonlibs/#" + value.lower() for value in failed_pipwin_packages)} for your Python version'
-                )
+            # since we don't know the dependencies here, repeat this process n number of times
+            # (worst case is `O(n)`, where the first package is dependant on all the others)
+            if len(failed_pipwin_packages) == 0:
+                break
+
+    missing_dependencies = missing_packages(DEPENDENCIES)
 
 try:
     try:
@@ -78,26 +122,7 @@ setup(
     packages=find_packages(),
     python_requires='>=3.6',
     setup_requires=['dunamai', 'setuptools>=41.2'],
-    install_requires=[
-        'adcircpy>=1.0.39',
-        'appdirs',
-        'bs4',
-        'click',
-        'coupledmodeldriver>=1.4.5',
-        'fiona',
-        'geopandas',
-        'matplotlib',
-        'nemspy>=0.6.16',
-        'netcdf4',
-        'numpy',
-        'pandas',
-        'pint',
-        'pint-pandas',
-        'pyproj>=2.6',
-        'python-dateutil',
-        'requests',
-        'shapely',
-    ],
+    install_requires=list(DEPENDENCIES),
     extras_require={
         'testing': ['pytest', 'pytest-cov', 'pytest-xdist', 'tables', 'wget'],
         'development': ['flake8', 'isort', 'oitnb'],
