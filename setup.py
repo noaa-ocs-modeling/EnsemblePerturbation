d0d445f963349f45094211c57107183dd671f6fa
zachary.burnett <zachary.burnett@noaa.gov>
2021-08-17 09:31:34 -0400

    update dependencies

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import config, find_packages, setup
 DEPENDENCIES = {
     'adcircpy>=1.0.39': ['gdal', 'fiona'],
     'appdirs': [],
-    'cartopy': [],
+    'cartopy': ['cython'],
     'cmocean': [],
     'bs4': [],
     'click': [],
