1af86e90f7fbef4a1c9d388a3ac83084be45151a
zachary.burnett <zachary.burnett@noaa.gov>
2021-09-13 10:46:13 -0400

    descriptive naming and linear colormap

--- examples/klpc_surrogate/run_kl.py
+++ examples/klpc_surrogate/run_kl.py
@@ -11,7 +11,6 @@ Otherwise, do your own downselection (sparsification) of the regional grid looki
 - Khachik
 """
 
-
 from matplotlib import pyplot
 import numpy
 
@@ -39,38 +38,38 @@ if __name__ == '__main__':
 
     numpy.nan_to_num(output, copy=False)
 
-    ymodel = output[:, ::100].T  # ymodel has a shape of ngrid x nens
-    ngrid, nens = ymodel.shape
+    model_results = output[:, ::100].T  # ymodel has a shape of ngrid x nens
+    num_nodes, num_ensemble_members = model_results.shape
 
     # average of fields
-    mean_vector = numpy.mean(ymodel, axis=1)
-    covariance = numpy.cov(ymodel)
+    mean_vector = numpy.mean(model_results, axis=1)
+    covariance = numpy.cov(model_results)
 
-    weights = trapezoidal_rule_weights(length=ngrid)
+    node_weights = trapezoidal_rule_weights(length=num_nodes)
 
     eigen_values, eigen_vectors = karhunen_loeve_eigen_values(
-        covariance=covariance, weights=weights
+        covariance=covariance, weights=node_weights
     )
 
     # Karhunen–Loève modes ('principal directions')
-    modes = karhunen_loeve_modes(eigen_vectors=eigen_vectors, weights=weights)
+    modes = karhunen_loeve_modes(eigen_vectors=eigen_vectors, weights=node_weights)
     eigen_values[eigen_values < 1.0e-14] = 1.0e-14
 
     relative_diagonal = karhunen_loeve_relative_diagonal(
         karhunen_loeve_modes=modes, eigen_values=eigen_values, covariance=covariance
     )
 
-    xi = karhunen_loeve_coefficient_samples(
-        data=ymodel, eigen_values=eigen_values, eigen_vectors=eigen_vectors,
+    coefficient_samples = karhunen_loeve_coefficient_samples(
+        data=model_results, eigen_values=eigen_values, eigen_vectors=eigen_vectors,
     )
 
-    xi = xi[:, ::-1]
+    coefficient_samples = coefficient_samples[:, ::-1]
     modes = modes[:, ::-1]
     eigen_values = eigen_values[::-1]
 
     if plot_eigen_values:
         pyplot.figure(figsize=(12, 9))
-        pyplot.plot(range(1, ngrid + 1), eigen_values, 'o-')
+        pyplot.plot(range(1, num_nodes + 1), eigen_values, 'o-')
         pyplot.gca().set_xlabel('x')
         pyplot.gca().set_ylabel('Eigenvalue')
         pyplot.savefig('eig.png')
@@ -88,9 +87,9 @@ if __name__ == '__main__':
         # clf()
 
         pyplot.figure(figsize=(12, 9))
-        pyplot.plot(range(ngrid), mean_vector, label='Mean')
-        for imode in range(ngrid):
-            pyplot.plot(range(ngrid), modes[:, imode], label='Mode ' + str(imode + 1))
+        pyplot.plot(range(num_nodes), mean_vector, label='Mean')
+        for imode in range(num_nodes):
+            pyplot.plot(range(num_nodes), modes[:, imode], label='Mode ' + str(imode + 1))
         pyplot.gca().set_xlabel('x')
         pyplot.gca().set_ylabel('KL Modes')
         pyplot.legend()
@@ -99,10 +98,10 @@ if __name__ == '__main__':
 
     # pick the first neig eigenvalues, look at rel_diag array or eig.png to choose how many eigenmodes you should pick without losing much accuracy
     # can go all the way to neig = ngrid, in which case one should exactly recover ypred = ymodel
-    neig = 25
-    xi = xi[:, :neig]
-    eigen_values = eigen_values[:neig]
-    modes = modes[:, :neig]
+    num_eigen_values = 25
+    coefficient_samples = coefficient_samples[:, :num_eigen_values]
+    eigen_values = eigen_values[:num_eigen_values]
+    modes = modes[:, :num_eigen_values]
 
     # Evaluate KL expansion using the same xi.
     #
@@ -110,15 +109,17 @@ if __name__ == '__main__':
     # Depends on your final goals, but the surrogate xi_pc and the associated ypred can be evaluated a lot more than 40 times and can be used for sensitivity analysis, moment extraction and model calibration.
     # Essentially you will have a KL+PC spatiotemporal surrogate approximation of your model.
     #
-    ypred = mean_vector + numpy.dot(
-        numpy.dot(xi, numpy.diag(numpy.sqrt(eigen_values))), modes.T
+    predicted_results = mean_vector + numpy.dot(
+        numpy.dot(coefficient_samples, numpy.diag(numpy.sqrt(eigen_values))), modes.T
     )
-    ypred = ypred.T
+    predicted_results = predicted_results.T
     # now ypred is ngrid x nens just like ymodel
 
     if plot_surrogate:
+        colormap = pyplot.get_cmap('gist_rainbow')
         # Plot to make sure ypred and ymodel are close
-        pyplot.plot(ymodel, ypred, 'o')
+        for index in range(len(model_results)):
+            pyplot.plot(model_results[index], predicted_results[index], 'o', c=colormap(index / len(model_results)))
         pyplot.plot([0, 6], [0, 6], '--k', label='1:1')
         pyplot.legend()
         pyplot.show()
