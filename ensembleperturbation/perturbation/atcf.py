4b93b25890bd7f469a5002ba68a6a93da7e6d3a3
zachary.burnett <zachary.burnett@noaa.gov>
2021-09-17 10:49:11 -0400

    don't use copy function in process

--- ensembleperturbation/perturbation/atcf.py
+++ ensembleperturbation/perturbation/atcf.py
@@ -1250,9 +1250,7 @@ class VortexPerturber:
                 dataframe[column] = dataframe[column].pint.magnitude
 
         # write out the modified `fort.22`
-        perturbed_forcing = copy(self.forcing)
-        perturbed_forcing.dataframe.loc[dataframe.index] = dataframe
-        perturbed_forcing.write(filename, overwrite=True)
+        VortexForcing(storm=dataframe).write(filename, overwrite=True)
 
         if weight is not None:
             perturbation['weight'] = weight
