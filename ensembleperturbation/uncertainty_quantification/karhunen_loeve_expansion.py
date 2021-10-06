c78ea156c31e61753c10351a66513ec2cfb8404f
William Pringle <wpringle@anl.gov>
2021-10-06 07:00:01 -0500

    Feature/chaospy additions from WPringle fork (#60)
    * adding run_klpc script to this branch with new options
    
    * some bugfixes
    
    * moving functions from run_klpc.py example into uncertainty_quantification directory
    
    * critical fix for ordering the eigenmodes
    
    * moving yprediction and plotting into the KL function from the example, adding ability to specify neig as decimal to automatically extract n modes that explain the desired percent (e.g., 0.99 for n top modes that explain 99% of the variance)
    
    * making polynomial chaos functions for calling the UQTk toolbox for command line, adding loop over xi columns to run_klpc.py
    
    * correcting function to call UQTk command line (needs UQTk installed)
    
    * separating KL prediction from the expansion, adding comparison of 2nd and 3rd order polynomial fit in the example
    
    * adding capabilitiy for computing the PC sensitivity and including in example
    
    * adding option for spacing sampling (can be slow, may need to revisit), trying out reducing sampling and removing any node that is NaN in at least one of the ensembles
    
    * correcting code to work with the update to the read_hdf function
    
    * adding savefigure and colorbar option to plot_points, correcting KL prediction to output ypred, adding plots for sobol sensitivities to the run_klpc.py example

--- ensembleperturbation/uncertainty_quantification/karhunen_loeve_expansion.py
+++ ensembleperturbation/uncertainty_quantification/karhunen_loeve_expansion.py
@@ -1,5 +1,104 @@
 import numpy
+from matplotlib import pyplot
 
+def karhunen_loeve_expansion(ymodel, neig = None, plot: bool = False):
+
+    # get the shape of the data
+    ngrid, nens = ymodel.shape
+    
+    if neig is None:
+        neig = ngrid
+    elif isinstance(neig,int):
+        neig = min(neig,ngrid)
+    elif isinstance(neig,float):
+        assert neig <= 1.0 and neig >= 0.0, 'specify 0.0 <= neig <= 1.0'
+
+    # evaluate weights and eigen values
+    mean_vector = numpy.mean(ymodel, axis=1)
+    covariance = numpy.cov(ymodel)
+
+    weights = trapezoidal_rule_weights(length=ngrid)
+
+    eigen_values, eigen_vectors = karhunen_loeve_eigen_values(
+        covariance=covariance, weights=weights
+    )
+
+    # Karhunen–Loève modes ('principal directions')
+    modes = karhunen_loeve_modes(eigen_vectors=eigen_vectors, weights=weights)
+    eigen_values[eigen_values < 1.0e-14] = 1.0e-14
+
+    relative_diagonal = karhunen_loeve_relative_diagonal(
+        karhunen_loeve_modes=modes, eigen_values=eigen_values, covariance=covariance
+    )
+
+    xi = karhunen_loeve_coefficient_samples(
+        data=ymodel,
+        eigen_values=eigen_values,
+        eigen_vectors=eigen_vectors,
+    )
+
+    # re-ordering the matrices (mode-1 first) 
+    xi = xi[:, ::-1]
+    modes = modes[:, ::-1]
+    eigen_values = eigen_values[::-1]
+  
+    # get desired modes 
+    if isinstance(neig,float):
+        # determine neig that make up neig decimal fraction of the variance explained
+        target = neig*sum(eigen_values)
+        eig_sum = 0
+        for neig in range(ngrid):
+            eig_sum = eig_sum + eigen_values[neig]
+            if eig_sum >= target:
+                break 
+        xi = xi[:, :neig]
+        eigen_values = eigen_values[:neig]
+        modes = modes[:, :neig]
+    else:
+        # get neig requested modes 
+        xi = xi[:, :neig]
+        eigen_values = eigen_values[:neig]
+        modes = modes[:, :neig]
+
+    if plot:
+        pyplot.figure(figsize=(12, 9))
+        pyplot.plot(range(1, neig+1), eigen_values, 'o-')
+        pyplot.gca().set_xlabel('x')
+        pyplot.gca().set_ylabel('Eigenvalue')
+        pyplot.savefig('eig.png')
+        pyplot.gca().set_yscale('log')
+        pyplot.savefig('eig_log.png')
+        pyplot.close()
+
+        pyplot.figure(figsize=(12, 9))
+        pyplot.plot(range(ngrid), mean_vector, label='Mean')
+        for imode in range(neig):
+            pyplot.plot(range(ngrid), modes[:, imode], label='Mode ' + str(imode + 1))
+        pyplot.gca().set_xlabel('x')
+        pyplot.gca().set_ylabel('KL Modes')
+        pyplot.legend()
+        pyplot.savefig('KLmodes.png')
+        pyplot.close()
+
+    return mean_vector, modes, eigen_values, xi
+
+def karhunen_loeve_prediction(
+    mean_vector, modes, eigen_values, xi, ymodel = None):
+    
+    # evaluating the model prediction based on neig modes
+    # now ypred is ngrid x nens just like ymodel
+    ypred = mean_vector + numpy.dot(numpy.dot(xi, numpy.diag(numpy.sqrt(eigen_values))), modes.T)
+    ypred = ypred.T
+        
+    if ymodel is not None:
+        # Plot to make sure ypred and ymodel are close
+        pyplot.plot(ymodel, ypred, 'o')
+        pyplot.gca().set_xlabel('actual')
+        pyplot.gca().set_ylabel('prediction')
+        pyplot.savefig('KLfit.png')
+        pyplot.close()
+
+    return ypred
 
 def trapezoidal_rule_weights(length: int):
     # Set trapesoidal rule weights
@@ -38,7 +137,6 @@ def karhunen_loeve_coefficient_samples(
     get samples for the Karhunen–Loève coefficients from the given data
 
     :param data: array of data of shape `ngrid x nens`
-    :param mean_vector: average of fields
     :param eigen_values: 1D array of eigen values
     :param eigen_vectors: 2D array of eigen vectors
     :return: samples for the Karhunen–Loève coefficients
---
+++ ensembleperturbation/uncertainty_quantification/polynomial_chaos.py
@@ -0,0 +1,67 @@
+import os
+
+def build_pc_expansion(
+    x_filename: str = 'xdata.dat', y_filename: str = 'qoi.dat', output_filename: str = None,
+    pc_type: str = 'HG', poly_order: int = 5, lambda_regularization: int = 0, 
+):
+    """
+    builds the polynomial-chaos expansion
+
+    regression function inputs: 
+    -x "xdata filename" 
+    -y "ydata filename" 
+    -s "PCtype" 
+    -o "Polynomial order"  
+    -l "regularization lambda"
+    """
+    uqtk_cmd = 'regression -x ' + x_filename + ' -y ' + y_filename + ' -s ' + pc_type + ' -o ' + str(poly_order) + ' -l ' + str(lambda_regularization)
+    os.system(uqtk_cmd)
+    if output_filename is not None:
+        os.rename('coeff.dat',output_filename)
+    return
+
+def evaluate_pc_expansion(
+    x_filename: str = 'xdata.dat', parameter_filename: str = 'coeff.dat', output_filename: str = None,
+    pc_type: str = 'HG', poly_order: int = 5, 
+):
+    """
+    evaluates the polynomial-chaos expansion
+    
+    pce_eval function inputs: 
+    -x "xdata filename" 
+    -f "parameter filename" 
+    -s "PC type" 
+    -o "Polynomial order"  
+    """
+    assert x_filename == 'xdata.dat', 'x_filename needs to be xdata.dat'
+    uqtk_cmd = 'pce_eval -f ' + parameter_filename + ' -s ' + pc_type + ' -o ' + str(poly_order)
+    os.system(uqtk_cmd)
+    if output_filename is not None:
+        os.rename('ydata.dat',output_filename)
+    return
+    
+def evaluate_pc_sensitivity(
+    parameter_filename: str = 'coeff.dat',
+    pc_type: str = 'HG', multiindex_type: str = 'TO', 
+    poly_order: int = 5, pc_dimension: int = 1, 
+):
+    """
+    evaluates the Sobol sensitivities of the PC expansion
+    
+    gen_mi function inputs (generates the multi-index files): 
+    -x "Multiindex type" 
+    -p "PC polynomial order" 
+    -q "PC dimension (number of parameters)" 
+    
+    pce_sens function inputs (generates the Sobol sensitivity indices): 
+    -x "PC type" 
+    -f "PC coefficient filename"
+    -m "Multiindex file (mindex.dat by default)"
+    """
+    # evaluate the multi-index file
+    uqtk_cmd = 'gen_mi -x ' + multiindex_type + ' -p ' + str(poly_order) + ' -q ' + str(pc_dimension)
+    os.system(uqtk_cmd)
+    
+    # evaluating the sensitivities
+    uqtk_cmd = 'pce_sens -f ' + parameter_filename + ' -x ' + pc_type
+    os.system(uqtk_cmd)
