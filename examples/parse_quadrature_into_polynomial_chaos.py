89fba75a77889a12be3a7c1e8c98059cd70aa26b
zachary.burnett <zachary.burnett@noaa.gov>
2021-10-13 13:44:55 -0400

    load surrogate from file

--- examples/parse_quadrature_into_polynomial_chaos.py
+++ examples/parse_quadrature_into_polynomial_chaos.py
@@ -14,88 +14,91 @@ if __name__ == '__main__':
     plot_storm = False
     plot_mean = True
     input_directory = Path.cwd()
-    output_filename = input_directory / 'surrogate.npy'
+    surrogate_filename = input_directory / 'surrogate.npy'
 
-    filenames = ['perturbations.nc', 'fort.63.nc']
+    if not surrogate_filename.exists():
+        filenames = ['perturbations.nc', 'fort.63.nc']
 
-    datasets = {}
-    existing_filenames = []
-    for filename in filenames:
-        filename = input_directory / filename
-        if filename.exists():
-            datasets[filename.name] = xarray.open_dataset(filename)
-            existing_filenames.append(filename.name)
+        datasets = {}
+        existing_filenames = []
+        for filename in filenames:
+            filename = input_directory / filename
+            if filename.exists():
+                datasets[filename.name] = xarray.open_dataset(filename)
+                existing_filenames.append(filename.name)
 
-    for filename in existing_filenames:
-        filenames.remove(filename)
+        for filename in existing_filenames:
+            filenames.remove(filename)
 
-    if len(filenames) > 0:
-        datasets.update(
-            combine_outputs(
-                input_directory,
-                file_data_variables=filenames,
-                maximum_depth=0,
-                only_inundated=True,
-                parallel=True,
+        if len(filenames) > 0:
+            datasets.update(
+                combine_outputs(
+                    input_directory,
+                    file_data_variables=filenames,
+                    maximum_depth=0,
+                    only_inundated=True,
+                    parallel=True,
+                )
+            )
+
+        perturbations = datasets['perturbations.nc']
+        elevations = datasets['fort.63.nc']
+
+        variables = {
+            variable_class.name: variable_class()
+            for variable_class in VortexPerturbedVariable.__subclasses__()
+        }
+
+        distribution = chaospy.J(
+            *(
+                variables[variable_name].chaospy_distribution()
+                for variable_name in perturbations['variable'].values
             )
         )
 
-    perturbations = datasets['perturbations.nc']
-    elevations = datasets['fort.63.nc']
+        # sample times and nodes
+        # TODO: sample based on sentivity / eigenvalues
+        sample_times = elevations['time'][::10]
+        sample_nodes = elevations['node'][::1000]
+        samples = elevations['zeta'].loc[{'time': sample_times, 'node': sample_nodes}]
+        # samples = elevations['zeta']
 
-    variables = {
-        variable_class.name: variable_class()
-        for variable_class in VortexPerturbedVariable.__subclasses__()
-    }
+        if plot_storm:
+            figure = pyplot.figure()
+            figure.suptitle(
+                f'standard deviation of {len(samples["node"])} max elevation(s) across {len(samples["run"])} run(s) and {len(samples["time"])} time(s)'
+            )
+            axis = figure.add_subplot(1, 1, 1)
 
-    distribution = chaospy.J(
-        *(
-            variables[variable_name].chaospy_distribution()
-            for variable_name in perturbations['variable'].values
+            countries = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+            countries.plot(color='lightgrey', ax=axis)
+
+            axis.scatter(samples['x'], samples['y'], c=samples.max('time').std('run'))
+
+            storm_name = 'florence2018'
+            storm = BestTrackForcing(storm_name)
+            storm.data.plot(x='longitude', y='latitude', label=storm_name, ax=axis)
+
+            pyplot.show()
+
+        # expand polynomials with polynomial chaos
+        polynomials = chaospy.generate_expansion(
+            order=3, dist=distribution, rule='three_terms_recurrence',
         )
-    )
 
-    # sample times and nodes
-    # TODO: sample based on sentivity / eigenvalues
-    sample_times = elevations['time'][::10]
-    sample_nodes = elevations['node'][::1000]
-    samples = elevations['zeta'].loc[{'time': sample_times, 'node': sample_nodes}]
-    # samples = elevations['zeta']
-
-    if plot_storm:
-        figure = pyplot.figure()
-        figure.suptitle(
-            f'standard deviation of {len(samples["node"])} max elevation(s) across {len(samples["run"])} run(s) and {len(samples["time"])} time(s)'
+        # create surrogate models for selected nodes
+        print(f'fitting surrogate to {samples.shape} samples')
+        surrogate_model = chaospy.fit_quadrature(
+            orth=polynomials,
+            nodes=perturbations['perturbations'].T.values,
+            weights=perturbations['weights'].values,
+            solves=samples,
         )
-        axis = figure.add_subplot(1, 1, 1)
-
-        countries = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
-        countries.plot(color='lightgrey', ax=axis)
-
-        axis.scatter(samples['x'], samples['y'], c=samples.max('time').std('run'))
-
-        storm_name = 'florence2018'
-        storm = BestTrackForcing(storm_name)
-        storm.data.plot(x='longitude', y='latitude', label=storm_name, ax=axis)
-
-        pyplot.show()
-
-    # expand polynomials with polynomial chaos
-    polynomials = chaospy.generate_expansion(
-        order=3, dist=distribution, rule='three_terms_recurrence',
-    )
-
-    # create surrogate models for selected nodes
-    print(f'fitting surrogate to {samples.shape} samples')
-    surrogate_model = chaospy.fit_quadrature(
-        orth=polynomials,
-        nodes=perturbations['perturbations'].T.values,
-        weights=perturbations['weights'].values,
-        solves=samples,
-    )
-    with open(output_filename, 'wb') as output_file:
-        print(f'saving surrogate model to "{output_filename}"')
-        surrogate_model.dump(output_file)
+        with open(surrogate_filename, 'wb') as surrogate_file:
+            print(f'saving surrogate model to "{surrogate_filename}"')
+            surrogate_model.dump(surrogate_file)
+    else:
+        surrogate_model = chaospy.load(surrogate_filename)
 
     # for surrogate_model in surrogate_models:
     mean = chaospy.E(poly=surrogate_model, dist=distribution)
