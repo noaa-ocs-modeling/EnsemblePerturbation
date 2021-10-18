95b4d44008cdfa8e22d0fe631d2bd40c62228903
zachary.burnett <zachary.burnett@noaa.gov>
2021-10-18 13:14:15 -0400

    add flexibility to plotting max elevation

--- examples/parse_quadrature_into_polynomial_chaos.py
+++ examples/parse_quadrature_into_polynomial_chaos.py
@@ -7,6 +7,7 @@ from matplotlib import pyplot
 from matplotlib.cm import get_cmap
 import numpy
 import xarray
+from xarray import DataArray
 
 from ensembleperturbation.parsing.adcirc import combine_outputs
 from ensembleperturbation.perturbation.atcf import VortexPerturbedVariable
@@ -67,19 +68,20 @@ if __name__ == '__main__':
 
     # sample times and nodes
     # TODO: sample based on sentivity / eigenvalues
-    # sample_times = elevations['time'][::10]
-    # sample_nodes = elevations['node'][::1000]
-    # samples = elevations['zeta'].loc[{'time': sample_times, 'node': sample_nodes}]
+    subsetted_times = elevations['time'][::10]
+    subsetted_nodes = elevations['node'][::1000]
+    # samples = elevations['zeta'].sel({'time': subsetted_times, 'node': subsetted_nodes})
     # samples = elevations['zeta']
-    samples = max_elevations['zeta_max']
+    samples = max_elevations['zeta_max'].sel({'node': subsetted_nodes})
+    # samples = max_elevations['zeta_max']
     LOGGER.info(f'sample size: {samples.shape}')
 
     if plot_storm:
         storm_figure = pyplot.figure()
-        message = f'standard deviation of {len(samples["node"])} max elevation(s) across {len(samples["run"])} run(s)'
+        title = f'standard deviation of {len(samples["node"])} max elevation(s) across {len(samples["run"])} run(s)'
         if 'time' in samples:
-            message = f'{message} and {len(samples["time"])} time(s)'
-        storm_figure.suptitle(message)
+            title = f'{title} and {len(samples["time"])} time(s)'
+        storm_figure.suptitle(title)
         axis = storm_figure.add_subplot(1, 1, 1)
 
         countries = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
@@ -109,14 +111,21 @@ if __name__ == '__main__':
         try:
             surrogate_model = chaospy.fit_quadrature(
                 orth=polynomials,
-                nodes=perturbations['perturbations'].T.values,
-                weights=perturbations['weights'].values,
-                solves=samples.sel(run=samples['run'] != 'original'),
+                nodes=perturbations['perturbations'].T,
+                weights=perturbations['weights'],
+                solves=samples,
             )
         except AssertionError:
-            raise AssertionError(
-                f'{perturbations["perturbations"].T.shape[1]} != {len(perturbations["weights"])} != {len(samples)}'
-            )
+            if (
+                len(perturbations['perturbations']['run'])
+                != len(perturbations['weights'])
+                != len(samples)
+            ):
+                raise AssertionError(
+                    f'{len(perturbations["perturbations"]["run"])} != {len(perturbations["weights"])} != {len(samples)}'
+                )
+            else:
+                raise
 
         with open(surrogate_filename, 'wb') as surrogate_file:
             LOGGER.info(f'saving surrogate model to "{surrogate_filename}"')
@@ -132,15 +141,26 @@ if __name__ == '__main__':
         reference_mean = samples.mean('run')
         reference_std = samples.std('run')
 
-        mean_figure = pyplot.figure()
-        mean_figure.suptitle(
-            f'surrogate-predicted and modeled means for {predicted_mean.shape[1]} nodes over {predicted_mean.shape[0]} times'
+        predicted_mean = DataArray(
+            predicted_mean, coords=reference_mean.coords, dims=reference_mean.dims
+        )
+        predicted_std = DataArray(
+            predicted_std, coords=reference_std.coords, dims=reference_std.dims
         )
 
-        std_figure = pyplot.figure()
-        std_figure.suptitle(
-            f'surrogate-predicted and modeled standard deviations for {predicted_mean.shape[1]} nodes over {predicted_mean.shape[0]} times'
+        mean_figure = pyplot.figure()
+        title = (
+            f'surrogate-predicted and modeled means for {len(predicted_mean["node"])} nodes'
         )
+        if 'time' in predicted_mean:
+            title = f'{title} over {len(predicted_mean["time"])} times'
+        mean_figure.suptitle(title)
+
+        std_figure = pyplot.figure()
+        title = f'surrogate-predicted and modeled standard deviations for {len(predicted_mean["node"])} nodes'
+        if 'time' in predicted_mean:
+            title = f'{title} over {len(predicted_mean["time"])} times'
+        std_figure.suptitle(title)
 
         colors = [
             get_cmap('gist_rainbow')(color_index / len(samples['node']))
@@ -157,42 +177,54 @@ if __name__ == '__main__':
         mean_difference_axis.set_title('differences')
         std_difference_axis.set_title('differences')
 
-        for node_index in range(len(samples['node'])):
-            color = colors[node_index]
+        if 'time' in samples:
+            for node_index, node in enumerate(samples['node'].values):
+                color = colors[node_index]
 
-            predicted_node_mean = predicted_mean[:, node_index]
-            predicted_node_std = predicted_std[:, node_index]
-            reference_node_mean = reference_mean[:, node_index]
-            reference_node_std = reference_std[:, node_index]
+                predicted_node_mean = predicted_mean.sel(node=node)
+                predicted_node_std = predicted_std.sel(node=node)
+                reference_node_mean = reference_mean.sel(node=node)
+                reference_node_std = reference_std.sel(node=node)
 
-            mean_value_axis.plot(samples['time'].values, predicted_node_mean, '--', c=color)
-            mean_value_axis.plot(samples['time'].values, reference_node_mean, c=color)
-            mean_value_axis.fill_between(
-                samples['time'].values,
-                predicted_node_mean - predicted_node_std,
-                predicted_node_mean + predicted_node_std,
-                color=color,
-                alpha=0.5,
+                mean_value_axis.plot(
+                    samples['time'].values, predicted_node_mean, '--', c=color
+                )
+                mean_value_axis.plot(samples['time'].values, reference_node_mean, c=color)
+                mean_value_axis.fill_between(
+                    samples['time'].values,
+                    predicted_node_mean - predicted_node_std,
+                    predicted_node_mean + predicted_node_std,
+                    color=color,
+                    alpha=0.5,
+                )
+                mean_value_axis.fill_between(
+                    samples['time'].values,
+                    reference_node_mean - reference_node_std,
+                    reference_node_mean + reference_node_std,
+                    color=color,
+                    alpha=0.25,
+                )
+
+                mean_difference_axis.plot(
+                    samples['time'].values,
+                    numpy.abs(reference_node_mean - predicted_node_mean),
+                    c=color,
+                )
+
+                std_value_axis.plot(samples['time'].values, reference_node_std, c=color)
+                std_value_axis.plot(samples['time'].values, predicted_node_std, '--', c=color)
+
+                std_difference_axis.plot(
+                    samples['time'].values, predicted_node_std - reference_node_std, c=color
+                )
+        else:
+            mean_value_axis.bar(predicted_mean['node'], predicted_mean)
+            mean_difference_axis.bar(
+                predicted_mean['node'], numpy.abs(predicted_mean - reference_mean)
             )
-            mean_value_axis.fill_between(
-                samples['time'].values,
-                reference_node_mean - reference_node_std,
-                reference_node_mean + reference_node_std,
-                color=color,
-                alpha=0.25,
-            )
-
-            mean_difference_axis.plot(
-                samples['time'].values,
-                numpy.abs(reference_node_mean - predicted_node_mean),
-                c=color,
-            )
-
-            std_value_axis.plot(samples['time'].values, reference_node_std, c=color)
-            std_value_axis.plot(samples['time'].values, predicted_node_std, '--', c=color)
-
-            std_difference_axis.plot(
-                samples['time'].values, predicted_node_std - reference_node_std, c=color
+            std_value_axis.bar(predicted_std['node'], predicted_std)
+            std_difference_axis.bar(
+                predicted_std['node'], numpy.abs(predicted_std - reference_std)
             )
 
         if save_plot:
@@ -200,33 +232,45 @@ if __name__ == '__main__':
             std_figure.savefig(input_directory / 'std.png', bbox_inches='tight')
 
     percentiles = [90]
-    percentile_filename = input_directory / 'percentiles.npy'
+    percentile_filename = input_directory / 'percentiles.nc'
     if not percentile_filename.exists():
         LOGGER.info(f'calculating {len(percentiles)} percentiles: {percentiles}')
         predicted_percentiles = chaospy.Perc(
             poly=surrogate_model, q=percentiles, dist=distribution, sample=samples.shape[1],
         )
         LOGGER.info(f'saving percentiles to "{percentile_filename}"')
-        numpy.save(str(percentile_filename), predicted_percentiles)
+
+        predicted_percentiles = DataArray(
+            predicted_percentiles,
+            coords={'percentile': percentiles, 'node': samples['node']},
+            dims=('percentile', 'node'),
+            name='percentiles',
+        )
+
+        predicted_percentiles.to_netcdf(percentile_filename)
     else:
         LOGGER.info(f'loading percentiles from "{percentile_filename}"')
-        predicted_percentiles = numpy.load(str(percentile_filename), allow_pickle=True)
+        predicted_percentiles = xarray.open_dataset(percentile_filename)
 
     if plot_percentile:
         percentile_figure = pyplot.figure()
 
-        for percentile_index, percentile_output in enumerate(predicted_percentiles):
+        for percentile_index, percentile_output in enumerate(
+            predicted_percentiles['percentiles']
+        ):
             axis = percentile_figure.add_subplot(len(percentiles), 1, percentile_index + 1)
 
-            percentile = percentiles[percentile_index]
+            if 'time' in percentile_output:
+                for node_index in range(percentile_output.shape[-1]):
+                    node_percentile = percentile_output[:, node_index]
+                    axis.plot(samples['time'].values, node_percentile)
+            else:
+                axis.bar(samples['node'], percentile_output)
 
-            for node_index in range(percentile_output.shape[1]):
-                node_percentile = percentile_output[:, node_index]
-                axis.plot(samples['time'].values, node_percentile)
-
-            percentile_figure.suptitle(
-                f'{percentile} percentile for {percentile_output.shape[1]} nodes over {percentile_output.shape[0]} times'
-            )
+            title = f'{percentile_output["percentile"].values} percentile for {len(percentile_output["node"])} nodes'
+            if 'time' in percentile_output:
+                title = f'{title} over {len(percentile_output["time"])} times'
+            percentile_figure.suptitle(title)
 
         if save_plot:
             percentile_figure.savefig(input_directory / 'percentiles.png', bbox_inches='tight')
