149ebf2ba041a9ffd934a7e26284e01f5fdaf1c7
zacharyburnett <zachary.r.burnett@gmail.com>
2021-10-28 14:14:11 -0400

    find quadrature runs

--- examples/parse_quadrature_into_polynomial_chaos.py
+++ examples/parse_quadrature_into_polynomial_chaos.py
@@ -349,7 +349,9 @@ if __name__ == '__main__':
     subset_bounds = (-83, 25, -72, 42)
     samples = max_elevations['zeta_max']
 
-    training_set = samples.sel(run='quadrature' in samples['run'])
+    training_set = samples.sel(
+        run=numpy.core.defchararray.find(samples['run'], 'quadrature') != -1
+    )
     training_set = training_set.sel(
         node=FieldOutput.subset(training_set['node'], bounds=subset_bounds),
     )
