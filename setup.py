3fc6051f0ba22912fe7e7c7818b5bac7ec78c3fd
zacharyburnettNOAA <zachary.burnett@noaa.gov>
2022-01-18 15:55:50 -0500

    fix python 3.6 importlib_metadata

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from collections.abc import Mapping
-from importlib import metadata as importlib_metadata
 import os
 from pathlib import Path
 import re
 import subprocess
 import sys
 from typing import Dict, List
+
+try:
+    from importlib import metadata as importlib_metadata
+except ImportError:  # for Python<3.8
+    subprocess.run(
+        f'{sys.executable} -m pip install importlib_metadata',
+        shell=True,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    import importlib_metadata
 
 from setuptools import config, find_packages, setup
 
