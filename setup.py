9503f072e15bde4c20e768850ad550ca138b9e8b
zacharyburnettNOAA <zachary.burnett@noaa.gov>
2022-03-02 16:32:11 -0500

    update build system

--- setup.py
+++ setup.py
@@ -1,101 +1,12 @@
-import os
-from pathlib import Path
-import subprocess
-import sys
+import warnings
 
-from setuptools import config, find_packages, setup
-
-DEPENDENCIES = [
-    'adcircpy>=1.1.0',
-    'appdirs',
-    'cartopy',
-    'chaospy',
-    'cmocean',
-    'bs4',
-    'coupledmodeldriver>=1.4.11',
-    'dask',
-    'fiona',
-    'geopandas',
-    'matplotlib',
-    'netcdf4',
-    'numpy',
-    'pandas',
-    'pint-pandas',
-    'pyproj>=2.6',
-    'tables',
-    'typepigeon',
-    'python-dateutil',
-    'requests',
-    'shapely',
-    'scikit-learn',
-    'stormevents',
-]
-
-if (Path(sys.prefix) / 'conda-meta').exists() or os.name == 'nt':
-    try:
-        import gartersnake
-
-        MISSING_DEPENDENCIES = gartersnake.missing_requirements(DEPENDENCIES)
-
-        if len(MISSING_DEPENDENCIES) > 0:
-            print(f'{len(MISSING_DEPENDENCIES)} (out of {len(DEPENDENCIES)}) dependencies are missing')
-
-        if len(MISSING_DEPENDENCIES) > 0 and gartersnake.is_conda():
-            gartersnake.install_conda_requirements(MISSING_DEPENDENCIES)
-            MISSING_DEPENDENCIES = gartersnake.missing_requirements(DEPENDENCIES)
-
-        if len(MISSING_DEPENDENCIES) > 0 and gartersnake.is_windows():
-            gartersnake.install_windows_requirements(MISSING_DEPENDENCIES)
-            MISSING_DEPENDENCIES = gartersnake.missing_requirements(DEPENDENCIES)
-    except:
-        pass
+from dunamai import Version
+from setuptools import find_packages, setup
 
 try:
-    try:
-        import dunamai
-    except ImportError:
-        subprocess.run(
-            f'{sys.executable} -m pip install dunamai',
-            shell=True,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
-
-    from dunamai import Version
-
     __version__ = Version.from_any_vcs().serialize()
-except (ModuleNotFoundError, RuntimeError) as error:
-    print(error)
+except RuntimeError as error:
+    warnings.warn(f'{error.__class__.__name__} - {error}')
     __version__ = '0.0.0'
 
-print(f'using version {__version__}')
-
-metadata = config.read_configuration('setup.cfg')['metadata']
-
-setup(
-    **metadata,
-    version=__version__,
-    packages=find_packages(),
-    python_requires='>=3.6',
-    setup_requires=['dunamai', 'setuptools>=41.2'],
-    install_requires=DEPENDENCIES,
-    extras_require={
-        'testing': ['pytest', 'pytest-cov', 'pytest-xdist', 'wget'],
-        'development': ['flake8', 'isort', 'oitnb'],
-        'documentation': [
-            'm2r2',
-            'sphinx',
-            'sphinx-rtd-theme',
-            'sphinxcontrib-programoutput',
-            'sphinxcontrib-bibtex',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-            'make_storm_ensemble=ensembleperturbation.client.make_storm_ensemble:main',
-            'perturb_tracks=ensembleperturbation.client.perturb_tracks:main',
-            'combine_results=ensembleperturbation.client.combine_results:main',
-            'plot_results=ensembleperturbation.client.plot_results:main',
-        ],
-    },
-)
+setup(version=__version__, packages=find_packages(exclude=('tests',)), test_suite='tests')
