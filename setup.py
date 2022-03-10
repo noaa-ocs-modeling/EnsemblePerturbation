451872faef01340b508a19f20ff8f827e16719ac
zacharyburnettNOAA <zachary.burnett@noaa.gov>
2022-03-10 15:07:38 -0500

    send warning for general exception when retrieving VCS-tagged version

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ from setuptools import find_packages, setup
 
 try:
     __version__ = Version.from_any_vcs().serialize()
-except:
+except Exception as error:
+    warnings.warn(f'{error.__class__.__name__} - {error}')
     __version__ = '0.0.0'
-#RuntimeError as error:
-#    warnings.warn(f'{error.__class__.__name__} - {error}')
-#    __version__ = '0.0.0'
 
 setup(version=__version__, packages=find_packages(exclude=('tests',)), test_suite='tests')
