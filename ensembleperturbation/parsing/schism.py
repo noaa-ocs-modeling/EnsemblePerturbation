a58f3dd80fb5f2c8d2948da346d352dae7d7d509
SorooshMani-NOAA <soroosh.mani@noaa.gov>
2022-07-01 13:44:06 -0400

    Format error

--- ensembleperturbation/parsing/schism.py
+++ ensembleperturbation/parsing/schism.py
@@ -703,7 +703,9 @@ class MaximumElevationOutput(MaximumScalarFieldOutputCalculator):
 
     @classmethod
     def _set_dry_to_null(cls, dataset: Dataset) -> Dataset:
-        dataset[cls.derived_name] = dataset[cls.derived_name].where(dataset[cls.derived_name] < dataset.attrs['h0'], numpy.nan)
+        dataset[cls.derived_name] = dataset[cls.derived_name].where(
+            dataset[cls.derived_name] < dataset.attrs['h0'], numpy.nan
+        )
 
         return dataset
 
