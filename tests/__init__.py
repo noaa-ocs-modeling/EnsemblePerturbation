8d6b1c141f6beba5a803b0ed065f55957d5c6acf
Soroosh.Mani <Soroosh.Mani@noaa.gov>
2023-07-11 09:45:15 -0400

    Fix lint error!

--- tests/__init__.py
+++ tests/__init__.py
@@ -85,6 +85,7 @@ def check_reference_directory(
 
                 cwd = Path.cwd()
                 diff = context_diff(test_lines, reference_lines, lineterm='')
-                assert '\n'.join(test_lines) == '\n'.join(
-                    reference_lines
-                ), f'"{os.path.relpath(test_filename, cwd)}" != "{os.path.relpath(reference_filename, cwd)}"\n\n' + '\n'.join(diff)
+                assert '\n'.join(test_lines) == '\n'.join(reference_lines), (
+                    f'"{os.path.relpath(test_filename, cwd)}" != "{os.path.relpath(reference_filename, cwd)}"\n\n'
+                    + '\n'.join(diff)
+                )
