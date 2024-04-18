20451bc3bfaa6e83776039a5ed00044b3de534d4
William Pringle <wpringle@anl.gov>
2024-04-18 13:09:37 -0500

    adding test for the online OFCL advisory using new RMW forecasts in StormEvents

--- tests/test_advisory_ensemble.py
+++ tests/test_advisory_ensemble.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pandas.testing
 
 from ensembleperturbation.perturbation.atcf import (
@@ -5,6 +6,7 @@ from ensembleperturbation.perturbation.atcf import (
     CrossTrack,
     MaximumSustainedWindSpeed,
     RadiusOfMaximumWindsPersistent,
+    RadiusOfMaximumWinds,
     perturb_tracks,
 )
 from tests import check_reference_directory, DATA_DIRECTORY
@@ -29,6 +31,36 @@ def test_existing_advisory():
         perturbations=9,
         directory=output_directory,
         storm=input_directory / 'florence_advisory_persistentRMW.22',
+        file_deck='a',
+        variables=variables,
+        sample_from_distribution=True,
+        sample_rule='korobov',
+        overwrite=True,
+        parallel=True,
+    )
+
+    check_reference_directory(output_directory, reference_directory)
+
+def test_online_advisory():
+    output_directory = DATA_DIRECTORY / 'output' / 'test_online_advisory'
+    reference_directory = DATA_DIRECTORY / 'reference' / 'test_online_advisory'
+
+    if not output_directory.exists():
+        output_directory.mkdir(parents=True, exist_ok=True)
+
+    variables = [
+        CrossTrack,
+        AlongTrack,
+        MaximumSustainedWindSpeed,
+        RadiusOfMaximumWinds,
+    ]
+
+    perturbations = perturb_tracks(
+        perturbations=9,
+        directory=output_directory,
+        storm='Ida2021',
+        start_date=datetime(2021,8,28),
+        advisories=['OFCL'],
         file_deck='a',
         variables=variables,
         sample_from_distribution=True,
