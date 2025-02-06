f379703e2eac4e4d50258f00d2d59d00ca0691ed
Soroosh Mani <77082694+SorooshMani-NOAA@users.noreply.github.com>
2025-02-06 06:44:33 -0500

    Remove scipy version upper limit (#169)
    * Remove scipy version upper limit
    
    * Update test to make macos optional
    
    * Make test comparison based on paresed json instead of file lines

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,14 @@
+from collections.abc import Collection
 from difflib import context_diff
+import json
+from numbers import Number
 import os
 from os import PathLike
 from pathlib import Path
 import re
 from typing import Dict, List
 
+import numpy as np
 import xarray
 
 DATA_DIRECTORY = Path(__file__).parent / 'data'
@@ -63,29 +67,55 @@ def check_reference_directory(
                 continue
 
             with open(test_filename) as test_file, open(reference_filename) as reference_file:
-                test_lines = list(test_file.readlines())
-                reference_lines = list(reference_file.readlines())
-
-                lines_to_skip = set()
-                for file_mask, line_indices in skip_lines.items():
-                    if (
-                        file_mask in str(test_filename)
-                        or re.match(file_mask, str(test_filename))
-                        and len(test_lines) > 0
-                    ):
-                        try:
-                            lines_to_skip.update(
-                                line_index % len(test_lines) for line_index in line_indices
-                            )
-                        except ZeroDivisionError:
-                            continue
-
-                for line_index in sorted(lines_to_skip, reverse=True):
-                    del test_lines[line_index], reference_lines[line_index]
-
                 cwd = Path.cwd()
-                diff = context_diff(test_lines, reference_lines, lineterm='')
-                assert '\n'.join(test_lines) == '\n'.join(reference_lines), (
-                    f'"{os.path.relpath(test_filename, cwd)}" != "{os.path.relpath(reference_filename, cwd)}"\n\n'
-                    + '\n'.join(diff)
-                )
+                errmsg = f'"{os.path.relpath(test_filename, cwd)}" != "{os.path.relpath(reference_filename, cwd)}"\n\n'
+                if test_filename.suffix == '.json' and reference_filename.suffix == '.json':
+                    test_json = json.load(test_file)
+                    reference_json = json.load(reference_file)
+                    try:
+                        diff_dicts(test_json, reference_json)
+                    except AssertionError as e:
+                        assert False, errmsg + str(e)
+                else:
+                    test_lines = list(test_file.readlines())
+                    reference_lines = list(reference_file.readlines())
+
+                    lines_to_skip = set()
+                    for file_mask, line_indices in skip_lines.items():
+                        if (
+                            file_mask in str(test_filename)
+                            or re.match(file_mask, str(test_filename))
+                            and len(test_lines) > 0
+                        ):
+                            try:
+                                lines_to_skip.update(
+                                    line_index % len(test_lines) for line_index in line_indices
+                                )
+                            except ZeroDivisionError:
+                                continue
+
+                    for line_index in sorted(lines_to_skip, reverse=True):
+                        del test_lines[line_index], reference_lines[line_index]
+
+                    diff = context_diff(test_lines, reference_lines, lineterm='')
+                    assert '\n'.join(test_lines) == '\n'.join(
+                        reference_lines
+                    ), errmsg + '\n'.join(diff)
+
+
+def diff_dicts(dict1, dict2):
+    assert dict1.keys() == dict2.keys()
+    for k1, v1 in dict1.items():
+        v2 = dict2[k1]
+        assert type(v1) == type(v2), 'Different key sets'
+
+        if isinstance(v1, Collection):
+            diff_dicts(v1, v2), f'{k1} is different {v1} != {v2}'
+            continue
+
+        elif isinstance(v1, Number):
+            if not np.isnan(v1) and not np.isnan(v2):
+                assert np.isclose(v1, v2), f'{k1} is different {v1} != {v2}'
+
+        else:
+            assert v1 == v2, f'{k1} is different {v1} != {v2}'
